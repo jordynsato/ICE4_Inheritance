2619a63c4baf0af2bcc5c5ccaf1b2a8f0453ab81
nmimura <nmimura@cpp.edu>
2018-02-16 15:41:18 -0800

    finished SeniorTicket and StudentTicket

--- src/Driver.java
+++ src/Driver.java
@@ -7,85 +7,85 @@ import java.util.Scanner;
  * @author Team Matcha: Serena Ing, Nana Mimura, Jordyn Sato
  */
 public class Driver {
-
-	//Fields
-	private static MovieTicket bp = new MovieTicket();
-	private static MovieTicket gs = new MovieTicket();
-	private static MovieTicket j = new MovieTicket();
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		boolean rerunFlag = true;
-		System.out.println("Welcome to Matcha Cinemas!");
-		
-		do {
-			//Gets the user option to buy or refund a ticket
-			boolean rerunOpt = true;
-			int option = 0;
-			do {
-				try {
-					System.out.println("Would you like to buy (1) or return (2) a ticket?");
-					if(scan.hasNextLine()) {
-						option = scan.nextInt();
-						if(option != 1 && option != 2) {
-							System.out.println("Please choose 1 or 2.");
-							scan.nextLine();
-							continue;
-						}
-						rerunOpt = false;
-					}
-				} catch(Exception e) {
-					System.out.println("Please enter in a valid option.");
-					scan.nextLine();
-					continue;
-				}
-			} while(rerunOpt);
-			
-			//Gets movie option
-			boolean rerunMovie = true;
-			int movie = 0;
-			do {
-				try {
-					System.out.println("Please enter the numbered movie you would like to see:");
-					System.out.println("Black Panther(1), The Greatest Showman(2), Jumanji(3)");
-					if(scan.hasNextLine()) {
-						movie = scan.nextInt();
-						if(movie != 1 && movie != 2 && movie != 3) {
-							System.out.println("Please choose a valid movie option.");
-							scan.nextLine();
-							continue;
-						}
-						rerunMovie = false;
-					}
-				} catch(Exception e) {
-					System.out.println("Please enter a valid option.");
-					scan.nextLine();
-					continue;
-				}
-			} while(rerunMovie);
-			
-			//When the user wants to buy a ticket
-			if(option == 1) {
-				System.out.println("What type of ticket would you like to buy?");
-				
-			}
-			//When the user wants to return a ticket
-			else {
-				System.out.println("What type of ticket are you returning?");
-				
-			}
-			
-			//Checks to see if the user wants to rerun the program
-			System.out.println("Would you like to rerun the program?");
-			String response = scan.next();
-			if(!response.equalsIgnoreCase("Y") && !response.equalsIgnoreCase("YES")) {
-				rerunFlag = false;
-				System.out.println("Thank you for using this program!");
-			}
-			scan.nextLine();
-		} while(rerunFlag);
-		scan.close();
-		System.out.println("End program");
-	}
-
+        
+        //Fields
+        private static MovieTicket bp = new MovieTicket();
+        private static MovieTicket gs = new MovieTicket();
+        private static MovieTicket j = new MovieTicket();
+        
+        public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                boolean rerunFlag = true;
+                System.out.println("Welcome to Matcha Cinemas!");
+                
+                do {
+                        //Gets the user option to buy or refund a ticket
+                        boolean rerunOpt = true;
+                        int option = 0;
+                        do {
+                                try {
+                                        System.out.println("Would you like to buy (1) or return (2) a ticket?");
+                                        if(scan.hasNextLine()) {
+                                                option = scan.nextInt();
+                                                if(option != 1 && option != 2) {
+                                                        System.out.println("Please choose 1 or 2.");
+                                                        scan.nextLine();
+                                                        continue;
+                                                }
+                                                rerunOpt = false;
+                                        }
+                                } catch(Exception e) {
+                                        System.out.println("Please enter in a valid option.");
+                                        scan.nextLine();
+                                        continue;
+                                }
+                        } while(rerunOpt);
+                        
+                        //Gets movie option
+                        boolean rerunMovie = true;
+                        int movie = 0;
+                        do {
+                                try {
+                                        System.out.println("Please enter the numbered movie you would like to see:");
+                                        System.out.println("Black Panther(1), The Greatest Showman(2), Jumanji(3)");
+                                        if(scan.hasNextLine()) {
+                                                movie = scan.nextInt();
+                                                if(movie != 1 && movie != 2 && movie != 3) {
+                                                        System.out.println("Please choose a valid movie option.");
+                                                        scan.nextLine();
+                                                        continue;
+                                                }
+                                                rerunMovie = false;
+                                        }
+                                } catch(Exception e) {
+                                        System.out.println("Please enter a valid option.");
+                                        scan.nextLine();
+                                        continue;
+                                }
+                        } while(rerunMovie);
+                        
+                        //When the user wants to buy a ticket
+                        if(option == 1) {
+                                System.out.println("What type of ticket would you like to buy?");
+                                
+                        }
+                        //When the user wants to return a ticket
+                        else {
+                                System.out.aprintln("What type of ticket are you returning?");
+                                
+                        }
+                        
+                        //Checks to see if the user wants to rerun the program
+                        System.out.println("Would you like to rerun the program?");
+                        String response = scan.next();
+                        if(!response.equalsIgnoreCase("Y") && !response.equalsIgnoreCase("YES")) {
+                                rerunFlag = false;
+                                System.out.println("Thank you for using this program!");
+                        }
+                        scan.nextLine();
+                } while(rerunFlag);
+                scan.close();
+                System.out.println("End program");
+        }
+        
 }
--- src/SeniorTicket.java
+++ src/SeniorTicket.java
@@ -1,4 +1,11 @@
 
-public class SeniorTicket extends AdultTicket {
+public class SeniorTicket extends AdultTicket implements Sellable {
         
+        public SeniorTicket() {
+                setTicketPrice(10.00);
+        }
+        
+        public double calculatePrice(int numTickets) {
+                return getTicketPrice() * numTickets;
+        }
 }
