d34e5286738cb1d38e1af5c047ac3cc23933a359
jordynsato <jmsato@cpp.edu>
2018-02-16 15:16:30 -0800

    Adding Inventory class

--- src/Driver.java
+++ src/Driver.java
@@ -8,6 +8,11 @@ import java.util.Scanner;
  */
 public class Driver {
 
+	//Fields
+	private static MovieTicket bp = new MovieTicket();
+	private static MovieTicket gs = new MovieTicket();
+	private static MovieTicket j = new MovieTicket();
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		boolean rerunFlag = true;
@@ -23,25 +28,64 @@ public class Driver {
 					if(scan.hasNextLine()) {
 						option = scan.nextInt();
 						if(option != 1 && option != 2) {
-							System.out.println("");
+							System.out.println("Please choose 1 or 2.");
+							scan.nextLine();
+							continue;
 						}
+						rerunOpt = false;
 					}
 				} catch(Exception e) {
-					
+					System.out.println("Please enter in a valid option.");
+					scan.nextLine();
+					continue;
 				}
 			} while(rerunOpt);
+			
 			//Gets movie option
 			boolean rerunMovie = true;
+			int movie = 0;
 			do {
 				try {
 					System.out.println("Please enter the numbered movie you would like to see:");
-					System.out.println("Black Panther(1), ");
+					System.out.println("Black Panther(1), The Greatest Showman(2), Jumanji(3)");
+					if(scan.hasNextLine()) {
+						movie = scan.nextInt();
+						if(movie != 1 && movie != 2 && movie != 3) {
+							System.out.println("Please choose a valid movie option.");
+							scan.nextLine();
+							continue;
+						}
+						rerunMovie = false;
+					}
 				} catch(Exception e) {
-					
+					System.out.println("Please enter a valid option.");
+					scan.nextLine();
+					continue;
 				}
 			} while(rerunMovie);
 			
+			//When the user wants to buy a ticket
+			if(option == 1) {
+				System.out.println("What type of ticket would you like to buy?");
+				
+			}
+			//When the user wants to return a ticket
+			else {
+				System.out.println("What type of ticket are you returning?");
+				
+			}
+			
+			//Checks to see if the user wants to rerun the program
+			System.out.println("Would you like to rerun the program?");
+			String response = scan.next();
+			if(!response.equalsIgnoreCase("Y") && !response.equalsIgnoreCase("YES")) {
+				rerunFlag = false;
+				System.out.println("Thank you for using this program!");
+			}
+			scan.nextLine();
 		} while(rerunFlag);
+		scan.close();
+		System.out.println("End program");
 	}
 
 }
