05371bd5b139dfb2ec9c6297326c78ef9a9a1fa1
Serena Ing <sming@cpp.edu>
2018-02-16 15:37:12 -0800

    serena hopefully doesn't mess up

--- src/AdultTicket.java
+++ src/AdultTicket.java
@@ -1,4 +1,22 @@
-
-public class AdultTicket {
-        
+/** 
+ * 
+ * @author Serena Ing, Jordyn Sato, Nana Mimura = Team Matcha
+ * 2/16/18
+ *
+ */
+public class AdultTicket extends MovieTicket implements Sellable{
+	private double price;
+	
+	public AdultTicket() {
+		price = 13.50;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	@Override public double calculatePrice(int quantity) {
+		return quantity * price;
+	}
+	
 }
--- src/SeniorTicket.java
+++ src/SeniorTicket.java
@@ -1,4 +1,16 @@
 
-public class SeniorTicket extends AdultTicket {
-        
+public class SeniorTicket extends AdultTicket implements Sellable{
+	private double price;
+	
+	public SeniorTicket() {
+		price = 10.00;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	
+	@Override public double calculatePrice(int quantity) {
+		return quantity * price;
+	}
 }
