e68685add4de670453216bf6c5fc6b2b7614050e
nmimura <nmimura@cpp.edu>
2018-02-16 15:59:08 -0800

    added comments in SeniorTicket

--- src/SeniorTicket.java
+++ src/SeniorTicket.java
@@ -1,4 +1,9 @@
-
+/**
+ * Last edit: 2/16/18 3:53pm
+ * @author Team Matcha
+ * 
+ * This class creates a new type of movie ticket called SeniorTicket which is a subclass of AdultTicket. The price for this ticket is $10.00.
+ **/
 public class SeniorTicket extends AdultTicket implements Sellable {
         
         public SeniorTicket() {
