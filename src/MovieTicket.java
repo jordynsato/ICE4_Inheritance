fd6725c41b270d5aed4ad2c5bfa2b097ea7b2802
nmimura <nmimura@cpp.edu>
2018-02-16 14:54:02 -0800

    finished MovieTicket class

--- src/MovieTicket.java
+++ src/MovieTicket.java
@@ -1,4 +1,29 @@
 
-public class MovieTicket {
-
+public abstract MovieTicket implements Sellable{
+        
+        private int inventory;
+        private String movie;
+        private static final int MAX_INVENTORY = 50;
+        
+        public MovieTicket() {
+                inventory = 0;
+                movie = "";
+        }
+        
+        public MovieTicket(String movie) {
+                this.movie = movie;
+                inventory = MAX_INVENTORY;
+        }
+        
+        public String getMovieName() {
+                return movie;
+        }
+        
+        public void setMovieName(String newMovie) {
+                movie = newMovie;
+        }
+        
+        public abstract refundTicket(int numTickets, double ticketPrice);
+        
+        public abstract sellTIcket(int numTickets);
 }
