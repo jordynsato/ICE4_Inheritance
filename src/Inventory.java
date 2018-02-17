c9ec8323c73c58e196f4fc8442cf89b828502402
nmimura <nmimura@cpp.edu>
2018-02-16 16:58:02 -0800

    removing arrayList and excess commits

--- src/Inventory.java
+++ src/Inventory.java
@@ -3,13 +3,10 @@ import java.util.*;
 public class Inventory {
         private final int MAXTICKETS = 50;
         private int remainingTickets;
-        //private double moneyMade;
-        private ArrayList<MovieTicket> ticketSoldList;
         
         public Inventory() {
                 remainingTickets = MAXTICKETS;
                 ticketSoldList = new ArrayList<MovieTicket>();
-                //moneyMade = 0;
         }
         
         public void refundTicket(int numOfTickets) throws RuntimeException {
@@ -18,23 +15,15 @@ public class Inventory {
                 }
                 else {
                         remainingTickets+= numOfTickets;
-                       //while(numOfTickets > 0) {
-                        	//ticketSoldList.remove(t);
-                        //}
                 }
         }
         
         public void sellTicket(int numOfTickets) throws RuntimeException {
                 if(remainingTickets-numOfTickets<0)
                         throw new RuntimeException();
-                //System.out.println("Error, please try again");
                 
                 else {
                         remainingTickets-= numOfTickets;
-                        //while(numOfTickets>0) {
-                        	//ticketSoldList.add(t);
-                        	//numOfTickets--;
-                        //}
                 }
         }
         
