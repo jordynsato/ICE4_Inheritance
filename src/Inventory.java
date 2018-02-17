4bad40ff4528fc309273d4be7f67fe542c727ee9
nmimura <nmimura@cpp.edu>
2018-02-16 16:27:26 -0800

    pushing because gitbash says to

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,24 +1,24 @@
 
 public class Inventory {
-	private final int MAXTICKETS = 50;
-	private int remainingTickets;
-	//private double moneyMade;
-	
-	public Inventory() {
-		remainingTickets = MAXTICKETS;
-		//moneyMade = 0;
-	}
-	
-	public void refundTicket(int numOfTickets) {
-		remainingTickets+= numOfTickets;
-		
-	}
-	
-	public void sellTicket(int numOfTickets) {
-		remainingTickets-= numOfTickets;
-	}
-	
-	public int getRemainingTickets() {
-		return remainingTickets;
-	}
+        private final int MAXTICKETS = 50;
+        private int remainingTickets;
+        //private double moneyMade;
+        
+        public Inventory() {
+                remainingTickets = MAXTICKETS;
+                //moneyMade = 0;
+        }
+        
+        public void refundTicket(int numOfTickets) {
+                remainingTickets+= numOfTickets;
+                
+        }
+        
+        public void sellTicket(int numOfTickets) {
+                remainingTickets-= numOfTickets;
+        }
+        
+        public int getRemainingTickets() {
+                return remainingTickets;
+        }
 }
